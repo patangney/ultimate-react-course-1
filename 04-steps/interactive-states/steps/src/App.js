4ca6e1787016a6a2fc2b84cb38957633852d2c57
Patrick Tangney <ptangney@outlook.com>
2024-06-12 11:59:26 +0100

    setup states

--- 04-steps/interactive-states/steps/src/App.js
+++ 04-steps/interactive-states/steps/src/App.js
@@ -1,10 +1,51 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+  // set state
+  const [step, setSteps] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setSteps(step - 1);
+    }
+  }
+  function handleNext() {
+    if (step < 3) {
+      setSteps(step + 1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step: {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
